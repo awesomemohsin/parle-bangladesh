7dc337d8c4b89aec9859c61367338e058cb9a855
awesomemohsin <mohsindude5@gmail.com>
2026-05-14 10:46:22 +0600

    fixed hydrations error

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} antialiased min-h-screen flex flex-col overflow-x-hidden`}>
         <AuthProvider>
           <CartProvider>
